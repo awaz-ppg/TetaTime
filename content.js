064ef8b159f318013aa46e3723a5de32aa98cbb3
Adrian Waz <awaz@ppg.com>
2019-11-13 10:19:37 +0100

    split periods for hours sum

--- content.js
+++ content.js
@@ -2,63 +2,82 @@
     TODO: better handling for multiple dom changes
 */
 
+chrome.storage.local.clear();
+
 document.body.addEventListener("DOMSubtreeModified", function() {
   var workTimeItems = document.getElementsByClassName("worktime-realization");
   if (workTimeItems.length) {
     //workHours = workTimeItems.map(x => x.innerText);
     var workHours = [];
-    for(var i = 0; i < workTimeItems.length; i++){
-        var hours = splitHours(workTimeItems[i].innerText);
-        if(hours){
-            workHours.push(hours);
-        }
-    };
+    for (var i = 0; i < workTimeItems.length; i++) {
+      var hours = splitHours(workTimeItems[i].innerText);
+      if (hours) {
+        workHours.push(hours);
+      }
+    }
     //console.log(workHours);
     timeDifferences = calcTimeDifferences(workHours);
-    //console.log(timeDifferences);
+    console.log(timeDifferences);
 
     overTime = calcOverTime(timeDifferences);
     //console.log(overTime);
 
-    chrome.storage.local.set({"overtime": overTime});
+    updateStorageData(overTime);
   }
 });
 
 function splitHours(innerText) {
-    if(innerText.indexOf('-') > 0){
-        var split = innerText.split("-");
-        for(var i = 0; i < split.length; i++)
-        {
-            split[i] = split[i].trim();
-        }
-        //console.log(split);
-        return split;
+  if (innerText.indexOf("-") > 0) {
+    var split = innerText.split("-");
+    for (var i = 0; i < split.length; i++) {
+      split[i] = split[i].trim();
     }
-    return;
+    //console.log(split);
+    return split;
+  }
+  return;
 }
 
-function calcTimeDifferences(workHoursArray){ // to be change to use work time
-    var timeDifferences = [];
-    for(var i = 0; i < workHoursArray.length; i++){
-        splitStart = workHoursArray[i][0].split(":");
-        splitEnd = workHoursArray[i][1].split(":");
+function calcTimeDifferences(workHoursArray) {
+  // to be change to use work time
+  var timeDifferences = [];
+  for (var i = 0; i < workHoursArray.length; i++) {
+    splitStart = workHoursArray[i][0].split(":");
+    splitEnd = workHoursArray[i][1].split(":");
 
-        var dateStart = new Date("2011/02/01");
-        dateStart.setHours(splitStart[0]);
-        dateStart.setMinutes(splitStart[1]);
+    var dateStart = new Date("2011/02/01");
+    dateStart.setHours(splitStart[0]);
+    dateStart.setMinutes(splitStart[1]);
 
-        var dateEnd = new Date("2011/02/01");
-        dateEnd.setHours(splitEnd[0]);
-        dateEnd.setMinutes(splitEnd[1]);
-        timeDifferences.push(Math.abs(dateEnd - dateStart)/60000); //in minutes
-    }
-    return timeDifferences;
+    var dateEnd = new Date("2011/02/01");
+    dateEnd.setHours(splitEnd[0]);
+    dateEnd.setMinutes(splitEnd[1]);
+    timeDifferences.push(Math.abs(dateEnd - dateStart) / 60000); //in minutes
+  }
+  return timeDifferences;
 }
 
-function calcOverTime(timeDifferences){
-    var overtime = 0;
-    for(var i = 0; i < timeDifferences.length; i++){
-        overtime += (timeDifferences[i] - 480); // to verify: what happens, when someone have 2 in&outs
+function calcOverTime(timeDifferences) {
+  var overtime = 0;
+  for (var i = 0; i < timeDifferences.length; i++) {
+    overtime += timeDifferences[i] - 480; // to verify: what happens, when someone have 2 in&outs ; check by time 
+  }
+  return overtime;
+}
+
+function updateStorageData(overTime) {
+    var url = window.location.href;
+    var month = url.substring(url.indexOf("month="), url.length);
+    var monthValue = month.substring(month.indexOf("=") + 1, month.indexOf("-"));
+
+    chrome.storage.local.get("overtime", function(data) {
+    var dataArray;
+    if (data.overtime == undefined) {
+      dataArray = [];
+    } else {
+        var dataArray = data.overtime;
     }
-    return overtime;
+    dataArray[monthValue] = overTime;
+    chrome.storage.local.set({ overtime: dataArray });
+  });
 }
--- popup.js
+++ popup.js
@@ -1,3 +1,48 @@
+var months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+
+var currentMonth = new Date().getMonth();
 chrome.storage.local.get("overtime", function(data) {
-    document.getElementById('overtime').innerHTML = data.overtime;
+  var result = "<ul>";
+  var sum = new Array(3).fill(0);
+  for (var i = 0; i < data.overtime.length; i++) {
+    if (data.overtime[i]) {
+      result += "<li>";
+      result += months[i];
+      result += ": ";
+      result += data.overtime[i];
+      result += " minutes</li>";
+    }
+    sum[Math.floor(i / 4)] += data.overtime[i];
+  }
+  result += "</ul>";
+  result += "Sum: ";
+  //result += sum;
+  result += "<ul>";
+  for (var i = 0; i < sum.length; i++) {
+    if (sum[i]) {
+      result += "<li>Period ";
+      result += i;
+      if (i == Math.floor(currentMonth / 4)) {
+        result += "(current)";
+      }
+      result += ": ";
+      result += sum[i];
+      result += " minutes</li>";
+    }
+  }
+  result += "</ul>";
+  document.getElementById("overtime").innerHTML = result;
 });
